72c138b3857f1f94ee542c497741424d3da54e85
Volker Gronau <vjay@gmx.net>
2017-09-20 13:56:17 +0200

    first commit

---
+++ unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  Windows;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+    movement: integer;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  movement := 1;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+//var
+// point: TPoint;
+begin
+  //GetCursorPos( point);
+  //point.x := point.x + movement;
+  //SetCursorPos(point.x,point.y);
+  mouse_event( MOUSEEVENTF_MOVE, movement, 0, 0, 0);
+  movement := movement * -1;
+end;
+
+end.
+
